c155127a0bb227e2bb11cbe647df76e8785c364f
Apopkov1996 <138931093+Apopkov1996@users.noreply.github.com>
2023-12-06 17:18:34 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App/App';
+
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { App } from 'components/App/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/rental-car">
     <App />
   </BrowserRouter>
 );
